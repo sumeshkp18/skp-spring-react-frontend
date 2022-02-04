23dc9ffa53d78d6c74aff659e5619e8d26d8a631
Sumesh KP <sumesh.kp@trenser.com>
2022-02-04 12:15:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [message, SetMessage] = useState("");
+
+  useState(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then(response => response.text())
+      .then(message => {
+        SetMessage(message)
+      });
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
@@ -15,7 +25,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React #1
         </a>
       </header>
     </div>
